b2e11bfbcff456fc5b9e71c62f6039ae244dd69a
liubin <2653322712@qq.com>
2018-07-12 17:48:56 +0800

    腾讯bugly热更新

--- StudySample/src/main/java/com/lester/lesterapp/MyApp.java
+++ StudySample/src/main/java/com/lester/lesterapp/MyApp.java
@@ -1,11 +1,16 @@
 package com.lester.lesterapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.StrictMode;
+import android.support.multidex.MultiDex;
 
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.Utils;
 import com.lester.lesterapp.receiver.MyReceiver;
 import com.lester.lesterapp.service.MyService;
 import com.lesterlau.base.BaseApplication;
-import com.lesterlau.base.keeplive.BaseService;
+import com.tencent.bugly.beta.Beta;
 
 public class MyApp extends BaseApplication {
     @Override
@@ -24,10 +29,29 @@ public class MyApp extends BaseApplication {
         startService(new Intent(this, MyService.class));
     }
 
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        Utils.init(this);
+        //使用腾讯bugly热更新
+        LogUtils.d("Beta attachBaseContext", "attachBaseContext");
+        // you must install multiDex whatever tinker is installed!
+        MultiDex.install(base);
+        // 安装tinker
+        Beta.installTinker();
+    }
+
     @Override
     protected void registerRebootReceiver() {
         super.registerRebootReceiver();
         keepLiveReveiver = new MyReceiver();
         registerReceiver(keepLiveReveiver, rebootIntentFilter);
     }
+
+    protected void setStrictMode() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyLog().build());
+    }
+
 }
